918181d38344b172dfc57574aac1a384eda54b57
JD Nafziger <nafziger10@gmail.com>
2023-08-10 13:27:10 -0500

    mandatory testing from readme.md completed on addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,7 +14,13 @@ function addToCollection(title,artist,yearPublished) {
     return album;
 }//end addToCollection
 
-addToCollection("When Bears Attack","Banjo and Kazooie",1998);
-addToCollection("Space Ghost Coast to Coast","Glass Animals",2022);
+console.log(addToCollection("When Bears Attack","Banjo and Kazooie",1998));
+console.log(addToCollection("Space Ghost Coast to Coast","Glass Animals",2022));
+console.log(addToCollection("Come to Brazil","Why Don't We",2020));
+
+console.log(addToCollection("Don't Wait", "Mapei", 2017));
+console.log(addToCollection("Feel It Still", "Portugal. The Man", 2018));
+console.log(addToCollection("Gold","Kiiara",2016));
+
 console.log(collection);
 
