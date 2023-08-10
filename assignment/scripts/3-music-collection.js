69c0a592d72772a272aba4b76f2d840a492a237f
JD Nafziger <nafziger10@gmail.com>
2023-08-10 13:50:12 -0500

    findByArtist() completed and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,15 +14,15 @@ function addToCollection(title,artist,yearPublished) {
     return album;
 }//end addToCollection
 
-console.log(addToCollection("When Bears Attack","Banjo and Kazooie","1998"));
-console.log(addToCollection("Space Ghost Coast to Coast","Glass Animals","2022"));
-console.log(addToCollection("Come to Brazil","Why Don't We","2020"));
-
-console.log(addToCollection("Don't Wait", "Mapei", "2017"));
-console.log(addToCollection("Feel It Still", "Portugal. The Man", "2018"));
-console.log(addToCollection("Gold","Kiiara","2016"));
-
-console.log(collection);
+function findByArtist(searchName){
+    let foundArtists = []; 
+    for (let i = 0; i < collection.length; i++){
+        if (collection[i].artist == searchName) {
+            foundArtists.push(collection[i]);
+        }
+    }
+    return foundArtists;
+}//end findByArtist
 
 function showCollection(collection){
     console.log("There are a total of",collection.length, "albums in the collection");
@@ -35,4 +35,21 @@ function showCollection(collection){
     }
 }//end showCollection()
 
+console.log(addToCollection("When Bears Attack","Banjo and Kazooie","1998"));
+console.log(addToCollection("Space Ghost Coast to Coast","Glass Animals","2022"));
+console.log(addToCollection("Come to Brazil","Why Don't We","2020"));
+console.log(addToCollection("Don't Wait", "Mapei", "2017"));
+console.log(addToCollection("Feel It Still", "Portugal. The Man", "2018"));
+console.log(addToCollection("Gold","Kiiara","2016"));
+console.log(addToCollection("Whippin","Kiiara","2019"))
+console.log(collection);
+
+
+console.log("********** showCollection() ********************")
 showCollection(collection);
+
+console.log("********** testing findByArtist() ********************")
+
+console.log(findByArtist("banana"));
+console.log(findByArtist("Portugal. The Man"));
+console.log(findByArtist("Kiiara"));
