78e04f0c5ec4a0268a1b2c9188ac3a4b697255dc
JD Nafziger <nafziger10@gmail.com>
2023-08-10 13:17:17 -0500

    addToCollection() added and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,20 @@
 // https://github.com/JDNafz/assignment-unit-5.3 
 
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title,artist,yearPublished) {
+    let album = {
+        title : title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}//end addToCollection
+
+addToCollection("When Bears Attack","Banjo and Kazooie",1998);
+addToCollection("Space Ghost Coast to Coast","Glass Animals",2022);
+console.log(collection);
+
